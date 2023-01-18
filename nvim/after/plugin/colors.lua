a5d102d690aefa60a5f276c4d1ca1822afa20e2f
Renzo Romeo <renzoromeo44@gmail.com>
2023-01-18 00:01:01 -0300

    transparent background

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,5 +2,5 @@ vim.cmd.colorscheme("gruvbox")
 vim.g.gruvbox_contrast_dark = "hard"
 
 -- Transparent background
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
